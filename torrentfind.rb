30142764479661a1085bf94d4b66d34372abcce1
Abhi Yerra <ykabhinav@gmail.com>
2008-09-20 18:52:40 -0700

    Added get_fav_pandora

--- torrentfind.rb
+++ torrentfind.rb
@@ -2,7 +2,12 @@
 #
 require 'rubygems'
 require 'hpricot'
+require 'rss/1.0'
+require 'rss/2.0'
 require 'open-uri'
+
+PandoraFav = 'http://feeds.pandora.com/feeds/people/ykabhinav/favorites.xml'
+LastfmFav = ''
 
 Search = ARGV.join ' '
 
@@ -21,4 +26,21 @@ def search_mininova
 
 end
 
-puts search_the_pirate_bay
+def get_fav_pandora
+	content = open(PandoraFav).read
+	rss = RSS::Parser.parse(content, false)
+
+	artists = []
+	rss.items.collect do |item|
+		song, artist = item.title.split(' by ')
+		artists << artist
+	end
+	artists = artists.sort.uniq
+	puts artists
+end
+
+def get_fav_lastfm
+
+end
+
+get_fav_pandora
