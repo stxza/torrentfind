c1a2f3772b2f8703d3a33e985c41dc47a1e17e49
Daniel <wodemoneke@gmail.com>
2008-12-16 15:21:30 +0200

    added piratebay function

--- torrentfind.py
+++ torrentfind.py
@@ -3,6 +3,22 @@
 import xmlparser
 import lastfmstats
 import string
+import webbrowser
+import urllib
+import urllib2
+
+def search_pirate_bay(album, num):
+    """album is a list (of lists) containing
+    [0] album rank on last.fm
+    [1] album name
+    [2] artist name
+    num is the max albums to search for each artist
+    """
+    
+    for i in range(0, num):
+        search = '%20'.join(album[i])
+        search_string = 'http://thepiratebay.org/search/%s/0/7/0' % search
+        webbrowser.open(search_string)
 
 def retrieve_xml_data(user, method, extra_data=None):
     lfm = lastfmstats.Lastfm_Stats()
@@ -24,6 +40,7 @@ def run():
     parser = xmlparser.XML_Parser(fname, ['name', 'name'])
     parser.run_iterator()
     loved_tracks = parser.collected
+
     #for each loved track, we need to get the top 5 albums of this artist
     for track in loved_tracks:
         #check our database and see if this artist has already been added
@@ -37,11 +54,13 @@ def run():
                 print 'please answer y or n'
         if answer == 'y':
             #add to database
-            fname = retrieve_xml_data(user, 'artist.getTopAlbums', {'artist' : track[1]})
-            parser = xmlparser.XML_Parser(fname, ['name', 'album'], {'album' : 'rank'})
+            artist_info = {'artist' : track[1]}
+            fname = retrieve_xml_data(user, 'artist.getTopAlbums', artist_info)
+            parser = xmlparser.XML_Parser(fname, ['name', 'album', 'name'], {'album' : 'rank'})
             parser.run_iterator()
             albums = parser.collected
-            print albums
+            #lets just get the top album for now
+            search_pirate_bay(albums, 1)
     
 if __name__ == '__main__':
     run()
